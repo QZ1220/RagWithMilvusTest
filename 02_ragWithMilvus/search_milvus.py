9bb4e47ca83ba8e19bdc6efea11f8286b880f4ea
QZ1220 <audi.car@qq.com>
2025-10-23 20:39:02 +0800

    modify code

--- 02_ragWithMilvus/search_milvus.py
+++ 02_ragWithMilvus/search_milvus.py
@@ -30,6 +30,9 @@ def emb_text(text):
         .embedding
     )
 
+######################
+# 基础搜索
+
 # 4、ANN搜索
 # 近似近邻（ANN）搜索以记录向量嵌入排序顺序的索引文件为基础
 # 根据接收到的搜索请求中携带的查询向量查找向量嵌入子集，将查询向量与子群中的向量进行比较，并返回最相似的结果
@@ -169,3 +172,69 @@ res = client.get(
 print("结果：")
 print(f"res:{res}")
 print()
+
+
+
+########################
+# 全文搜索
+
+# # 4、分词测试
+# # 通用分词分析器
+# # analyzer_params = {"tokenizer": "standard", "filter": ["lowercase"]}
+# # analyzer_params = {"type": "standard"}
+# # 适合中文的分词分析器
+analyzer_params = {"tokenizer": "jieba", "filter": ["cnalphanumonly"]}
+# analyzer_params = {"type": "chinese"}
+# # 分词测试
+# # text = "An efficient system relies on a robust analyzer to correctly process text for various applications."
+# # text = "We-Math 2.0：全新多模态数学推理数据集 × 首个综合数学知识体系"
+# # text = "AI大咖齐聚！共议「人工智能+」国家战略落地路径"
+text = "鼎桥通信是国内著名的通信设备生产商"
+result = client.run_analyzer(
+    text,
+    analyzer_params
+)
+print("分词结果：")
+print(f"result:{result}")
+print()
+
+# 5、全文搜索
+# 全文搜索是一种通过匹配文本中特定关键词或短语来检索文档的传统方法
+# 它根据术语频率等因素计算出的相关性分数对结果进行排序
+# 语义搜索更善于理解含义和上下文，而全文搜索则擅长精确的关键词匹配，因此是语义搜索的有益补充
+# 对title进行全文搜索
+question = "鼎桥公司"
+res = client.search(
+    collection_name="job_postings",
+    anns_field="embedding",  ## 这里其实使用这个字段不对，应该单独创建一个SparseFloatVector类型的索引，明白就行，这里就不再修改。
+    data=[question],
+    limit=3,
+    search_params={
+        'params': {'drop_ratio_search': 0.2},
+    },
+    output_fields=["id", "title", "job_name", "job_salary", "job_edu"],
+)
+retrieved_lines_with_distances = [
+    (res["entity"]["title"], res["entity"]["job_name"], res["entity"]["job_salary"], res["entity"]["job_edu"], res["distance"]) for res in res[0]
+]
+print("分词查询的问题："+question)
+print("结果：")
+print(json.dumps(retrieved_lines_with_distances, indent=4, ensure_ascii=False))
+print()
+
+# # 对content进行全文搜索
+# question = "AI智能体"
+# res = client.search(
+#     collection_name="my_collection_demo_chunked",
+#     anns_field="content_sparse",
+#     data=[question],
+#     limit=3,
+#     search_params={
+#         'params': {'drop_ratio_search': 0.2},
+#     },
+#     output_fields=["title", "content_chunk", "link", "pubAuthor"]
+# )
+# retrieved_lines_with_distances = [
+#     (res["entity"]["title"], res["entity"]["content_chunk"], res["entity"]["link"], res["entity"]["pubAuthor"], res["distance"]) for res in res[0]
+# ]
+# print(json.dumps(retrieved_lines_with_distances, indent=4, ensure_ascii=False))
